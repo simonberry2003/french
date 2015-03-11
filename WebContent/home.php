fe060dbbe5844f57faab3596e32d5ea940f9ed2e
sberry <simon_berry2003@yahoo.com>
2015-03-11 21:32:56 +1030

    Added footer

--- WebContent/home.php
+++ WebContent/home.php
@@ -32,34 +32,37 @@
 		<div class="panel-heading">
 			<h3 class="panel-title">French Translation Services</h3>
 		</div>
-		<div class="panel-body"><p>Lorem ipsum dolor sit amet, at nam
-			cotidieque eloquentiam, eum nonumy menandri ad, solet maluisset
-			corrumpit vix te. Eu amet tractatos vis. Meliore gubergren an quo.
-			Pro graeci iriure et. Impetus dolorum in usu, eam utinam delicata
-			repudiare in, at eum commodo assueverit. 
-			<p>Paulo impedit accusamus id
-			pro, duo purto ponderum luptatum id. Eu ornatus gloriatur ullamcorper
-			vim, duo vocibus voluptatum scripserit an. Te timeam labitur eos, id
-			interesset complectitur disputationi per, rebum legimus veritus mei
-			ad. An sea exerci detracto placerat. Ad veniam vocibus sit, sumo
-			fabellas no usu. Duo senserit forensibus no, ea probo ferri nonumes
-			ius. Mea an atqui libris intellegam. 
-			<p>Omnes appellantur sit ut, an per
-			aliquid salutandi. Intellegat contentiones nam te. Mei ad euismod
-			eruditi, nam ad eleifend electram definiebas, per in ferri summo
-			utinam. Nec ne cibo accusata complectitur. Ius modo intellegat
-			comprehensam te, habeo legendos definitionem cum ne, vel te etiam
-			reprimique. Sed amet apeirian postulant id, has ei wisi mollis, eum
-			partem ornatus reprehendunt ad. 
-			<p>Ei summo saperet accusamus sea, in
-			has option repudiare consequat, semper vocent discere usu ei. Eam
-			nonumes verterem et. In omnium sapientem appellantur quo, dicit
-			oblique complectitur pro et. Eirmod regione mea et. Labore nusquam
-			sed eu. Te eos malis quando ubique, nec errem doctus mediocrem ne. Cu
-			vix elitr semper dolorum. 
-			<p>Pro fabulas facilis ullamcorper te. Per ea
-			omnesque instructior suscipiantur, vim ei nibh nostrud pertinacia.
-			Usu appareat vivendum an, ius vituperata quaerendum at.</div>
+		<div class="panel-body">
+			<p>Lorem ipsum dolor sit amet, at nam cotidieque eloquentiam, eum
+				nonumy menandri ad, solet maluisset corrumpit vix te. Eu amet
+				tractatos vis. Meliore gubergren an quo. Pro graeci iriure et.
+				Impetus dolorum in usu, eam utinam delicata repudiare in, at eum
+				commodo assueverit.
+			<p>Paulo impedit accusamus id pro, duo purto ponderum luptatum
+				id. Eu ornatus gloriatur ullamcorper vim, duo vocibus voluptatum
+				scripserit an. Te timeam labitur eos, id interesset complectitur
+				disputationi per, rebum legimus veritus mei ad. An sea exerci
+				detracto placerat. Ad veniam vocibus sit, sumo fabellas no usu. Duo
+				senserit forensibus no, ea probo ferri nonumes ius. Mea an atqui
+				libris intellegam.
+			<p>Omnes appellantur sit ut, an per aliquid salutandi. Intellegat
+				contentiones nam te. Mei ad euismod eruditi, nam ad eleifend
+				electram definiebas, per in ferri summo utinam. Nec ne cibo accusata
+				complectitur. Ius modo intellegat comprehensam te, habeo legendos
+				definitionem cum ne, vel te etiam reprimique. Sed amet apeirian
+				postulant id, has ei wisi mollis, eum partem ornatus reprehendunt
+				ad.
+			<p>Ei summo saperet accusamus sea, in has option repudiare
+				consequat, semper vocent discere usu ei. Eam nonumes verterem et. In
+				omnium sapientem appellantur quo, dicit oblique complectitur pro et.
+				Eirmod regione mea et. Labore nusquam sed eu. Te eos malis quando
+				ubique, nec errem doctus mediocrem ne. Cu vix elitr semper dolorum.
+			
+			<p>Pro fabulas facilis ullamcorper te. Per ea omnesque
+				instructior suscipiantur, vim ei nibh nostrud pertinacia. Usu
+				appareat vivendum an, ius vituperata quaerendum at.
+		</div>
+		<div class="panel-footer">email:xxx@hotmail.com</div>
 	</div>
 </body>
 </html>
