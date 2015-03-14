6fcfaa6a819d03caa72ccda17f32ec9cef303ac7
sberry <simon_berry2003@yahoo.com>
2015-03-14 23:13:05 +1030

    Fixed footer

--- WebContent/menu.php
+++ WebContent/menu.php
@@ -1,11 +1,15 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
             <a class="navbar-brand" href="home.php">FTS</a>
         </div>
-
-        <div class="collapse navbar-collapse"
-            id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li <?php echo ($page == 'home') ? 'class="active"' : '';?>><a href="home.php">Home</a></li>
                 <li <?php echo ($page == 'services') ? 'class="active"' : '';?>><a href="services.php">Services</a></li>
